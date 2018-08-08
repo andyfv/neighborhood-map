9497ea706f0f1a2b5ab31323f2bdc6a038802685
Andy <andyfilchev@gmail.com>
2018-08-08 23:40:28 +0300

    feat: Set initial logic for getting and distributing Foursquare venues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MapContainer from './MapContainer'
+import SearchBar from './SearchBar'
+import * as FoursquareAPI from './utils/FoursquareAPI'
 import './App.css';
 
 class App extends Component {
+  state = {
+    isMapLoaded: false,
+    venues: [],
+    center: {
+      lat: 42.696430,
+      lng: 23.321032
+    }
+  }
+
+hasMapLoaded = (boolean) => {
+  this.setState({
+    isMapLoaded: true
+  })
+}
+
+componentDidMount() {
+  if(!this.state.venues.length) {
+    FoursquareAPI.fetchVenues(this.state.center)
+    .then(venues => this.setState({venues: venues}));
+  }
+}
+
+listVenues() {
+  console.log(this.state.venues.map(venue => venue.name))
+}
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar/>
+        <MapContainer hasMapLoaded={this.hasMapLoaded}/>
       </div>
     );
   }
 }
 
-export default App;
+export default App
