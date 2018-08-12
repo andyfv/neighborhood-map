9645482e90db7357682ae88c623dafbcc2635fc4
Andy <andyfilchev@gmail.com>
2018-08-12 21:46:52 +0300

    feat: Add initial logic for MapContainer

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,33 +1,98 @@
 import React, { Component } from 'react'
 import scriptLoader from 'react-async-script-loader'
-import {G_MAPS_KEY} from './data/credentials'
-
-// const AnyReactComponent = ({text}) => <div>{text}</div>;
+import { G_MAPS_KEY } from './data/credentials'
+import * as FoursquareAPI from './utils/FoursquareAPI' 
+import memoize from 'memoize-one'
+import InfoWindow from './InfoWindow'
 
 class MapContainer extends Component {
 
     state = {
-        'center': {lat: 42.696430,lng: 23.321032},
-        'zoom': 12.5,
-        isMapLoaded: false
+        zoom: 14,
+        isMapLoaded: false,
+        venues: [],
+        map: '',
+        infoWindow: '',
+        markers: []
     }
 
-    componentDidUpdate() {
-        if(this.props.isScriptLoaded === true && (this.state.isMapLoaded !== true)) {
-            let map = new window.google.maps.Map(document.getElementById('map'),{
+    componentDidUpdate(prevProps) {
+        console.log('update');
+        
+        this.didMapLoaded();
+        this.didVenuesUpdate(prevProps);
+    }
+
+    didMapLoaded() {
+        if (this.props.isScriptLoaded && !this.state.isMapLoaded) {
+            let map = new window.google.maps.Map(document.getElementById('map'), {
                 zoom: this.state.zoom,
-                center: this.state.center
+                center: this.props.center
             })
-                this.setState({isMapLoaded: true});
-                this.props.hasMapLoaded(true);
+            this.setState({
+                isMapLoaded: true,
+                map: map,
+                infoWindow: new window.google.maps.InfoWindow()
+            });
+        }
+    }
+
+    didVenuesUpdate(prevProps) {
+        if(JSON.stringify(this.props.venues) !== JSON.stringify(prevProps.venues)){
+            if(this.state.isMapLoaded) {
+                let markersArr = [];
+                let venuesMap = new Map();
+                this.props.venues.forEach(venue => {
+                    markersArr.push([venue.id,this.createMarker(venue)])
+                    venuesMap.set(venue.id, venue)})
+                this.setState({
+                    markers: new Map(markersArr),
+                    venues: venuesMap
+                })
+            }
+        }
+    }
+
+    createMarker(venue) {
+        let venuePosition = {lat: venue.location.lat, lng: venue.location.lng};
+        let marker = new window.google.maps.Marker({
+            position: venuePosition,
+            map: this.state.map,
+            id: venue.id});
+        marker.addListener('click', () => this.openInfoWindow(venue.id, venue));
+        return marker;
+    }
+
+    toggleBounce(id) {
+        let marker = this.state.markers.get('4c1b93ce8b3aa593fc61975f');
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(window.google.maps.Animation.BOUNCE);
         }
     }
 
 
+    // Add try ...catch around the markers.get()
+    openInfoWindow(id, venue) {
+        console.log(id);
+        console.log(this.state.venues);
+        console.log(this.state.infoWindow);
+        this.state.markers.get(id).setAnimation(window.google.maps.Animation.BOUNCE)
+        if( this.state.infoWindow !== '') {
+            
+        } else {
+            console.log('bounce');
+            
+            <InfoWindow venue={this.state.venues}/>
+            this.state.markers.get(id).setAnimation(window.google.maps.Animation.BOUNCE);
+        }
+    }
     
     render () {
-        console.log(this.props);
-        console.log(this.state)
+        
+        console.log(this.state.venues);
+        
         return (
             <div id="map"></div>
         )
