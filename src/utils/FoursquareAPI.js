ccfbbb2760b14a84a0da28ca96aac7cbfa609528
Andy <andyfilchev@gmail.com>
2018-08-14 23:17:20 +0300

    feat: Update the return from fetching specific Venues Details

--- src/utils/FoursquareAPI.js
+++ src/utils/FoursquareAPI.js
@@ -2,7 +2,7 @@ import { CLIENT_ID, CLIENT_SECRET } from '../data/credentials'
 
 // Foursqare API Parameters
 const API = 'https://api.foursquare.com/v2/venues/';
-const RADIUS = 6000;
+const RADIUS = 3000;
 const LIMIT = 50;
 const VERSION = 20180708;
 const CATEGORIES = {
@@ -13,7 +13,7 @@ const CATEGORIES = {
 const CATEGORY_ARR = Object.keys(CATEGORIES).map((category) => CATEGORIES[category]);
 
 // Fetch venues based on @center parameter and the predefined Foursquare API paramaters
-export const fetchVenues = (center) => {
+export const fetchVenues = (center, query) => {
     let url = `${API}search?
                 ll=${center.lat},${center.lng}&
                 client_id=${CLIENT_ID}&
@@ -21,6 +21,7 @@ export const fetchVenues = (center) => {
                 categoryId=${CATEGORY_ARR}&
                 v=${VERSION}&
                 radius=${RADIUS}&
+                query=${query}&
                 limit=${LIMIT}`;
 
     
@@ -40,12 +41,19 @@ export const fetchVenues = (center) => {
 export const fetchDetails = (venueID) => {
     let url = `${API}
                 ${venueID}?
-                &client_id=${CLIENT_ID}&
+                client_id=${CLIENT_ID}&
                 client_secret=${CLIENT_SECRET}&
                 v=${VERSION}`;
 
     return fetch(formatURL(url))
-        .then(response => {console.log(response.json())})
+        .then(response => {
+            if(!response.ok) {
+                throw response
+            } else {
+                return response.json()
+            }
+        })
+        .then(data => Promise.resolve(data.response.venue))
 }
 
 // Correctly format the URL by removing white spaces, indentation, etc.
