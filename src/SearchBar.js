5a2c0defaf5032ab90d0ca7e9e32347e52919260
Andy <andyfilchev@gmail.com>
2018-08-14 23:11:53 +0300

    feat: Add handle when query updates and delete button

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
 
+    handleQueryChange = (value) => {
+        this.props.updateQuery(value)
+    }
+
+    clearInput = () => {
+        let input = document.getElementsByClassName('search-input')[0];
+        input.value = '';
+        this.handleQueryChange(input.value);
+    }
+
     render () {
         return (
             <div className="search-bar">
                 <svg id="search-icon" />
                 <div className="search-input-wrapper">
-                    <input className="search-input" type="text"/>
-                    <button id="delete-icon">
-                    </button>
+                    <input className="search-input" type="text" 
+                        onChange={(e) => this.handleQueryChange(e.target.value)}/>
+                    <button id="delete-button" onClick={this.clearInput}></button>
                 </div>
             </div>
         )
