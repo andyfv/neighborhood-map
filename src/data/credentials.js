66976e38dac3fffb7c2ad200f554fa23140f9740
Andy <andyfilchev@gmail.com>
2018-08-17 02:39:39 +0300

    docs: Add doc

--- src/data/credentials.js
+++ src/data/credentials.js
@@ -1,7 +1,12 @@
+/* 
+    File used to store the needed credentials
+ */
+
 // Foursquare Keys
 export const CLIENT_ID = 'FIK5FRXELDBEVMS503P4TACGXSCPWN5FUO0SBISJLK3DFC1C';
 
 export const CLIENT_SECRET = 'TD0EZ2QQTAR2XXMAZDTNY1P33JYLOUKIK1KZZNAA0PPLBPSO';
+
 
 // Google Maps Key
 export const G_MAPS_KEY = 'AIzaSyBwRwoWoCF7HTe95FPvTpWCqgvJkcgqBZI';
