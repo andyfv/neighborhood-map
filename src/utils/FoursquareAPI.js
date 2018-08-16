303f8f9f53bf2a47ec82ad9f8a03ba410a83c513
Andy <andyfilchev@gmail.com>
2018-08-17 02:42:55 +0300

    style: Formatting code

--- src/utils/FoursquareAPI.js
+++ src/utils/FoursquareAPI.js
@@ -1,4 +1,7 @@
-import { CLIENT_ID, CLIENT_SECRET } from '../data/credentials'
+import {
+    CLIENT_ID,
+    CLIENT_SECRET
+} from '../data/credentials'
 
 // Foursqare API Parameters
 const API = 'https://api.foursquare.com/v2/venues/';
@@ -8,11 +11,11 @@ const VERSION = 20180708;
 const CATEGORIES = {
     'Food': '4d4b7105d754a06374d81259',
 }
- 
+
 // Return only the ID's from @CATEGORIES and asign them to @CATEGORY_ARR
 const CATEGORY_ARR = Object.keys(CATEGORIES).map((category) => CATEGORIES[category]);
 
-// Fetch venues based on @center parameter and the predefined Foursquare API paramaters
+// Fetch venues based on @center and @query parameters and the predefined Foursquare API paramaters
 export const fetchVenues = (center, query) => {
     let url = `${API}search?
                 ll=${center.lat},${center.lng}&
@@ -24,11 +27,11 @@ export const fetchVenues = (center, query) => {
                 query=${query}&
                 limit=${LIMIT}`;
 
-    
+
     return fetch(formatURL(url))
         .then(response => {
-            if(!response.ok) {
-                throw response
+            if (!response.ok) {
+                throw Error('Unable to fetch Venues')
             } else {
                 return response.json()
             }
@@ -45,10 +48,11 @@ export const fetchDetails = (venueID) => {
                 client_secret=${CLIENT_SECRET}&
                 v=${VERSION}`;
 
+
     return fetch(formatURL(url))
         .then(response => {
-            if(!response.ok) {
-                throw response
+            if (!response.ok) {
+                throw Error('Unable to fetch Venue Details')
             } else {
                 return response.json()
             }
@@ -57,6 +61,6 @@ export const fetchDetails = (venueID) => {
 }
 
 // Correctly format the URL by removing white spaces, indentation, etc.
-function formatURL (URL) {
+function formatURL(URL) {
     return URL.replace(/\s+/g, '');
 }
--- src/utils/InfoWindow.js
+++ src/utils/InfoWindow.js
@@ -1,42 +1,34 @@
-
 export const content = (venue) => {
     console.log(venue);
 
-    try {
-        const unsafeOptions = {
-            name: venue.name,
-            category: venue.categories[0].name,
-            address: venue.location.address,
-            hours: venue.hours.status !== undefined ? venue.hours.status: '',
-            contacts: venue.contact.formattedPhone,
-            url: venue.shortUrl
-        };
-    
-        const defaultOptions = {
-            name: '',
-            category: '',
-            address: '',
-            hours: '',
-            contacts: '',
-            url: ''
-        };
-    
-        const options = {
-            ...unsafeOptions,
-            ...defaultOptions
-        };
+    const unsafeOptions = {
+        name: venue.name,
+        category: venue.categories[0].name,
+        address: venue.location.address,
+        contacts: venue.contact ? venue.contact.formattedPhone : '',
+        url: venue.shortUrl ? venue.shortUrl : ''
+    };
 
-        return `<div class="info-window">
-                    <h2 class="info-heading">${options.name}</h2>
-                    <h3 class="info-category">${options.category}</h3>
-                    <p class="info-address">${options.address}</p>
-                    <p class="info-hours">${options.hours}</p>
-                    <p class="info-contacts">${options.contacts}</p>
-                    <p class="info-url">${options.url}</p>
-                </div>`
-                
-    } catch (error) {
-        console.log(error);
-        
-    }
+    const defaultOptions = {
+        name: '',
+        category: '',
+        address: '',
+        hours: '',
+        contacts: '',
+        url: ''
+    };
+
+    const options = {
+        ...defaultOptions,
+        ...unsafeOptions
+    };
+
+    return `<div class="info-window" tab-index="0">
+                <h2 class="info-heading">${options.name}</h2>
+                <h3 class="info-category">${options.category}</h3>
+                <p class="info-address">${options.address}</p>
+                <p class="info-contacts">${options.contacts}</p>
+                <a  href="${options.url}" class="info-url">${options.url}</a>
+            </div>`
+
 }
